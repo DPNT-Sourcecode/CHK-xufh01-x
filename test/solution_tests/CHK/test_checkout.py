402b8ebfbcc58e63f20300b4d46352f265eb8a74
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-02-20 08:43:23 +0000

    Wed Feb 20 08:43:23 GMT 2019

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -4,8 +4,13 @@ from lib.solutions.CHK.checkout_solution import checkout
 
 
 class TestCheckout(unittest.TestCase):
+    """
+    This class contains unit tests for the checkout function
+
+    The tests below are built 
+    """
     def test_item_A(self):
-        self.assertEquals(checkout("A"), 180)
+        self.assertEquals(checkout("AAA"), 180)
 
 
 if __name__ == '__main__':
