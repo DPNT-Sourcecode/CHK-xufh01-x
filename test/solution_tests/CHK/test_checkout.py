8a2cbd7cfed60536bc323e026af5ffa758620b02
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-02-20 07:40:23 +0000

    Wed Feb 20 07:40:23 GMT 2019

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,7 +1,12 @@
 import unittest
+
 from lib.solutions.CHK import checkout_solution
 
 
-class TestCheckout(unittest.testcase):
+class TestCheckout(unittest.TestCase):
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
+
+if __name__ == '__main__':
+    unittest.main()
