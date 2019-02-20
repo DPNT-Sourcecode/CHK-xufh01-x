8a064afb47e745340cbd7b5f1143db90ec621fff
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-02-20 08:46:23 +0000

    Wed Feb 20 08:46:23 GMT 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,21 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    return 1
+    """
+    This class contains unit tests for the checkout function.
+
+    The tests below are built based on the following criteria:
+        +------+-------+----------------+
+        | Item | Price | Special
+        offers |
+        +------+-------+----------------+
+        | A | 50 | 3A for 130 |
+        | B | 30 | 2B for 45 |
+        | C | 20 | |
+        | D | 15 | |
+        +------+-------+----------------+
+
+        Notes:
+        - For any illegal input return -1
+    """
+
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,17 +1,48 @@
 import unittest
-
 from lib.solutions.CHK.checkout_solution import checkout
 
 
 class TestCheckout(unittest.TestCase):
     """
-    This class contains unit tests for the checkout function
+    This class contains unit tests for the checkout function.
 
-    The tests below are built 
+    The tests below are built based on the following criteria:
+        +------+-------+----------------+
+        | Item | Price | Special
+        offers |
+        +------+-------+----------------+
+        | A | 50 | 3A for 130 |
+        | B | 30 | 2B for 45 |
+        | C | 20 | |
+        | D | 15 | |
+        +------+-------+----------------+
+
+        Notes:
+        - For any illegal input return -1
     """
+
     def test_item_A(self):
-        self.assertEquals(checkout("AAA"), 180)
+        self.assertEqual(checkout("AAAA"), 180)
+
+    def test_item_B(self):
+        self.assertEqual(checkout("BBB"), 75)
+
+    def test_item_C(self):
+        self.assertEqual(checkout("C"), 20)
+
+    def test_item_D(self):
+        self.assertEqual(checkout("D"), 15)
+
+    def test_mixed_items(self):
+        self.assertEqual(checkout("ABCD"), 115)
+
+    def test_illegal_input(self):
+        self.assertEqual(checkout("ABCDZ"), -1)
+
+    def test_no_items(self):
+        self.assertEqual(checkout(""), 0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
+
