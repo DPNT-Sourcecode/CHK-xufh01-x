3190704d88f8ff655cb7a45011a5165fe551ed0f
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-02-20 12:16:28 +0000

    Wed Feb 20 12:16:28 GMT 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -92,7 +92,24 @@ def checkout(skus):
 
     DEALS_FOR_DIFFERENT_ITEM_FREE = {
         { "E", 2, "B" },
-        {}
+        { "N", 3, "M" },
+        { "R", 3, "Q" }
+    }
+
+    DEALS_FOR_BETTER_PRICE = {
+        { "A", 5, 200 },
+        { "A", 3, 130 },
+        { "B", 2, 45 },
+        { "F", 3, 20 },
+        { "H", 10, 80 },
+        { "H", 5, 45 },
+        { "K", 2, 150 },
+        { "P", 5, 200 },
+        { "Q", 3, 80 },
+        { "U", 4, 120 },
+        { "V", 3, 130 },
+        { "V", 2, 90 },
+
     }
 
     itemCount = {}
@@ -137,6 +154,7 @@ def checkout(skus):
         return -1
     else:
         return totalPrice
+
 
 
 
