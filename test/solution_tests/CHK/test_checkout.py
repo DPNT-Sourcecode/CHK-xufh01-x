31f35ea95ed8136468ac57921e96a82b580e3750
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-02-20 07:43:23 +0000

    Wed Feb 20 07:43:23 GMT 2019

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -4,9 +4,10 @@ from lib.solutions.CHK import checkout_solution
 
 
 class TestCheckout(unittest.TestCase):
-    def test_sum(self):
-        assert sum_solution.compute(1, 2) == 3
+    def test_item_A(self):
+        self.assertEquals(checkout_solution("A"), 180)
 
 
 if __name__ == '__main__':
     unittest.main()
+
