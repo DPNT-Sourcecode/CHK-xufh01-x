1a6686742d86313e7347fe9ffcd5944f8d2f651e
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-02-20 11:46:28 +0000

    Wed Feb 20 11:46:28 GMT 2019

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -105,42 +105,48 @@ class TestCheckout(unittest.TestCase):
     def test_illegal_item(self):
         self.assertEqual(checkout("ABCDEZ"), -1)
 
+
+if __name__ == "__main__":
+    testCheckout = TestCheckout()
+    unittest.main()
+
 class standardPrices(TestCheckout):
-    
-    standardPrices = {
-        "A": 50,
-        "B": 30,
-        "C": 20,
-        "D": 15,
-        "E": 40,
-        "F": 10,
-        "G": 20,
-        "H": 10,
-        "I": 35,
-        "J": 60,
-        "K": 80,
-        "L": 90,
-        "M": 15,
-        "N": 40,
-        "O": 10,
-        "P": 50,
-        "Q": 30,
-        "R": 50,
-        "S": 30,
-        "T": 20,
-        "U": 40,
-        "V": 50,
-        "W": 20,
-        "X": 90,
-        "Y": 10,
-        "Z": 50
-    }
-    for item, price in STANDARD_PRICES.items():
-        self.assertEquals(checkout(item), price)
+    def __init__(self):
+        standardPrices = {
+            "A": 50,
+            "B": 30,
+            "C": 20,
+            "D": 15,
+            "E": 40,
+            "F": 10,
+            "G": 20,
+            "H": 10,
+            "I": 35,
+            "J": 60,
+            "K": 80,
+            "L": 90,
+            "M": 15,
+            "N": 40,
+            "O": 10,
+            "P": 50,
+            "Q": 30,
+            "R": 50,
+            "S": 30,
+            "T": 20,
+            "U": 40,
+            "V": 50,
+            "W": 20,
+            "X": 90,
+            "Y": 10,
+            "Z": 50
+        }
+        for item, price in standardPrices.items():
+            self.assertEquals(checkout(item), price)
 
 
 if __name__ == "__main__":
     unittest.main()
+
 
 
 
